1748caa6881c7c8c3d40bb5b827fd7dfb3e5da4c
João Pedro Maturo Sofalo <102874668+joao-maturo@users.noreply.github.com>
2022-06-13 13:15:27 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,18 @@
+from re import L
+
+# mais impressão
+A = 'c'
+b = 'h'
+c = 'e'
+d = 'e'
+E = 's'
+f = 'e'
+g = 'b'
+h = 'u'
+i = 'r'
+j = 'g'
+k = 'e'
+L = 'r'
+
+print(A + b + c + d + E + f, end='')
+print(g + h + i + j + k + L)
